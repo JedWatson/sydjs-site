6af4f6ff9a8fed19da831a4ede0ce049a571bcff
Joss Mackison <joss.mackison@gmail.com>
2014-06-16 16:53:18 +1000

    Formatting the about page

--- routes/views/about.js
+++ routes/views/about.js
@@ -8,10 +8,10 @@ exports = module.exports = function(req, res) {
 	locals.section = 'about';
 	
 	locals.organisers = [
-		{ name: 'Craig Sharkie',    image: '/images/organiser-craig_sharkie.jpg',    twitter: 'twalve',            title: 'Founder, coordinator, ticket wrangler, emcee, and chief herder' },
-		{ name: 'Gil Davidson',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Site coordinator' },
-		{ name: 'Adam Ahmed',       image: '/images/organiser-adam_ahmed.jpg',       twitter: 'hitsthings',        title: 'Atlassian coordinator' },
-		{ name: 'Lachlan Hardy',     image: '/images/organiser-lachlan_hardy.jpg',   twitter: 'lachlanhardy',      title: 'Community coordinator' }
+		{ name: 'Craig Sharkie', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Founder, MC, coordinator' },
+		{ name: 'The Thinkmill', image: '/images/organiser-thinkmill.jpg',     twitter: 'thethinkmill', title: 'Site coordinator' },
+		{ name: 'Adam Ahmed',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
+		{ name: 'Lachlan Hardy', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Community coordinator' }
 	]
 	
 	view.render('site/about');
