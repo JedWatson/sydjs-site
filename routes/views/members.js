16fab938318f2c6123f06f11f6cfd7f803977f5c
Joss Mackison <joss.mackison@gmail.com>
2014-06-18 11:48:49 +1000

    Break members/mentors into separate routes

---
+++ routes/views/members.js
@@ -0,0 +1,70 @@
+var keystone = require('keystone'),
+	_ = require('underscore');
+
+var User = keystone.list('User');
+
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res),
+		locals = res.locals;
+	
+	locals.section = 'members';
+
+
+	// Load Organisers
+	
+	view.on('init', function(next) {
+		User.model.find()
+		.sort('name.first')
+		.where('isPublic', true)
+		.where('isOrganiser', true)
+		.exec(function(err, organisers) {
+			if (err) res.err(err);
+			locals.organisers = organisers;
+			next();
+		});
+	});
+
+
+	// Load Speakers
+
+	view.on('init', function(next) {
+		User.model.find()
+		.sort('-talkCount name.first')
+		.where('isPublic', true)
+		.where('talkCount').gt(0)
+		.exec(function(err, speakers) {
+			if (err) res.err(err);
+			locals.speakers = speakers;
+			next();
+		});
+	});
+
+
+	// Pluck IDs for filtering Community
+
+	view.on('init', function(next) {
+		locals.organiserIDs = _.pluck(locals.organisers, 'id');
+		locals.speakerIDs = _.pluck(locals.speakers, 'id');
+		next();
+	});
+
+
+	// Load Community
+
+	view.on('init', function(next) {
+		User.model.find()
+		.sort('-lastRSVP')
+		.where('isPublic', true)
+		.where('_id').nin(locals.organiserIDs)
+		.where('_id').nin(locals.speakerIDs)
+		.exec(function(err, community) {
+			if (err) res.err(err);
+			locals.community = community;
+			next();
+		});
+	});
+
+
+	view.render('site/members');
+}
