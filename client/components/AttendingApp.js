3de853f709395cc2d0cb5eea8ccbd0509212a908
Jed Watson <jed.watson@me.com>
2015-05-20 18:28:56 +1000

    Updating scripts to match new RSVP Store structure

--- client/components/AttendingApp.js
+++ client/components/AttendingApp.js
@@ -6,34 +6,37 @@ var AttendingApp = React.createClass({
 
 	getInitialState: function() {
 		return {
-			loaded: RSVPStore.isLoaded(),
+			isReady: RSVPStore.isLoaded(),
 			attendees: RSVPStore.getAttendees()
 		};
 	},
 
 	componentDidMount: function() {
-		RSVPStore.addChangeListener(this.updateAttendees);
+		RSVPStore.addChangeListener(this.updateStoreFromState);
 	},
 
-	updateAttendees: function() {
+	componentWillUnmount: function() {
+		RSVPStore.removeChangeListener(this.updateStoreFromState);
+	},
+
+	updateStoreFromState: function() {
 		this.setState({
+			isReady: RSVPStore.isLoaded(),
 			attendees: RSVPStore.getAttendees()
 		});
 	},
 	
 	renderHeading: function() {
-		if (!this.state.attendees) return <h3 className="heading-with-line">...</h3>;
+		if (!this.state.isReady) return <h3 className="heading-with-line">...</h3>;
 		var count = this.state.attendees ? this.state.attendees.length : '...';
 		var plural = count === 1 ? ' person is' : ' people are';
 		return <h3 className="heading-with-line"> {count + plural} attending</h3>;
 	},
 
 	render: function() {
-		var results = this.state.attendees;
-		var numberAttending = results.length
-		var resultsList;
-		if (results) {
-			resultsList = results.map(function(person) {
+		var attendeesList;
+		if (this.state.isReady) {
+			attendeesList = this.state.attendees.map(function(person) {
 				return <li key={person.id}><a href={person.url}><img width="40" height="40" alt={person.name} className="img-circle" src={person.photo ? person.photo : "/images/avatar.png"} /></a></li>
 			});
 		}
@@ -41,9 +44,9 @@ var AttendingApp = React.createClass({
 			<div>
 				<section className="attending">
 					{this.renderHeading()}
-					{<ul className="list-unstyled list-inline text-center attendees-list">
-						{resultsList}
-					</ul>}
+					<ul className="list-unstyled list-inline text-center attendees-list">
+						{attendeesList}
+					</ul>
 				</section>
 			</div>
 		);
--- client/components/HeroApp.js
+++ client/components/HeroApp.js
@@ -6,20 +6,26 @@ var HeroApp = React.createClass({
 
 	getInitialState: function() {
 		return {
-			meetup: SydJS.meetup,
 			user: SydJS.user,
-			rsvpStatus: SydJS.rsvpStatus,
-			attendees: false
+			isReady: RSVPStore.isLoaded(),
+			meetup: RSVPStore.getMeetup(),
+			rsvp: RSVPStore.getRSVP()
 		};
 	},
 
 	componentDidMount: function() {
-		RSVPStore.addChangeListener(this.updateAttendees);
+		RSVPStore.addChangeListener(this.updateStateFromStore);
 	},
 
-	updateAttendees: function() {
+	componentWillUnmount: function() {
+		RSVPStore.removeChangeListener(this.updateStoreFromState);
+	},
+
+	updateStateFromStore: function() {
 		this.setState({
-			attendees: RSVPStore.getAttendees()
+			isReady: RSVPStore.isLoaded(),
+			meetup: RSVPStore.getMeetup(),
+			rsvp: RSVPStore.getRSVP()
 		});
 	},
 
@@ -33,57 +39,71 @@ var HeroApp = React.createClass({
 		});
 	},
 
-	welcomeMessage: function() {
-		if (this.state.rsvpStatus.attending) {
+	renderWelcome: function() {
+		if (this.state.rsvp.attending) {
 			return <h4 className="hero-button-title"><span className = "welcome-message">See you there!</span></h4>
 		} else {
-			return <h4 className="hero-button-title">Are you coming? <br /> <span className="spots-left">{this.state.meetup.remainingRSVPs - this.state.attendees.length}<span className="text-thin"> spots left</span></span><br /></h4>
+			return <h4 className="hero-button-title">Are you coming? <br /> <span className="spots-left">{this.state.meetup.remainingRSVPs}<span className="text-thin"> spots left</span></span><br /></h4>
+		}
+	},
+
+	renderLoading: function() {
+		return (
+			<div className="hero-button">
+				<div className="alert alert-success mb-0 text-center">loading...</div>
+			</div>
+		);
+	},
+
+	renderSignedIn: function() {
+		var attending = this.state.rsvp.attending ?  ' btn-success btn-default active' : null;
+		var notAttending = this.state.rsvp.attending ? null : ' btn-danger btn-default active';
+		if(this.state.meetup.rsvpsAvailable || this.state.rsvp.rsvped && this.state.rsvp.attending) {
+			return (
+				<div>
+					{this.renderWelcome()}
+					<div className="hero-button">
+						<div id="next-meetup" data-id={this.state.meetup._id} className="form-row meetup-toggle">
+							<div className="col-xs-6">
+								<button type="button" onClick={this.sendRSVP.bind(this, true)} className={"btn btn-lg btn-block btn-default js-rsvp-attending" + attending}>Yes</button>
+							</div>
+							<div className="col-xs-6">
+								<button type="button" onClick={this.sendRSVP.bind(this, false)} className={"btn btn-lg btn-block btn-default js-rsvp-decline" + notAttending}>No</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div className="hero-button">
+					<div className="alert alert-success mb-0 text-center">No more tickets...</div>
+				</div>
+			);
+		}
+	},
+
+	renderSignedOut: function() {
+		if (this.state.meetup.rsvpsAvailable) {
+			return (
+				<div className="hero-button">
+					<a className="btn btn-primary btn-lg btn-block js-auth-trigger">RSVP Now (<span class="text-thin">{this.state.meetup.remainingRSVPs} spots left</span>)</a>
+				</div>
+			)
+		} else {
+			return (
+				<div className="hero-button">
+					<div className="alert alert-success mb-0 text-center">No more tickets...</div>
+				</div>
+			);
 		}
 	},
 
 	render: function() {
-		var attendees =  this.state.attendees
-		if (this.state.user) {
-			var attending = this.state.rsvpStatus.attending ?  ' btn-success btn-default active' : null
-			var notAttending = this.state.rsvpStatus.attending ? null : ' btn-danger btn-default active'
-			if(this.state.meetup.rsvpsAvailable || this.state.rsvpStatus.rsvped && this.state.rsvpStatus.attending) {
-				return (
-					<div>
-						{this.welcomeMessage()}
-						<div className="hero-button">
-							<div id="next-meetup" data-id={this.state.meetup._id} className="form-row meetup-toggle">
-								<div className="col-xs-6">
-									<button type="button" onClick={this.sendRSVP.bind(this, true)} className={"btn btn-lg btn-block btn-default js-rsvp-attending" + attending}>Yes</button>
-								</div>
-								<div className="col-xs-6">
-									<button type="button" onClick={this.sendRSVP.bind(this, false)} className={"btn btn-lg btn-block btn-default js-rsvp-decline" + notAttending}>No</button>
-								</div>
-							</div>
-						</div>
-					</div>
-				);
-			} else {
-				return (
-					<div className="hero-button">
-						<div className="alert alert-success mb-0 text-center">No more tickets...</div>
-					</div>
-				);
-			}
-		} else {
-			if (this.state.meetup.rsvpsAvailable) {
-				return (
-					<div className="hero-button">
-						<a className="btn btn-primary btn-lg btn-block js-auth-trigger">RSVP Now {this.state.meetup.remainingRSVPs} spots left<span class="text-thin"></span></a>
-					</div>
-				)
-			} else {
-				 return (
-					<div className="hero-button">
-						<div className="alert alert-success mb-0 text-center">No more tickets...</div>
-					</div>
-				);
-			}
+		if (!this.state.isReady) {
+			return this.renderLoading();
 		}
+		return this.state.user ? this.renderSignedIn() : this.renderSignedOut();
 	},
 });
 
