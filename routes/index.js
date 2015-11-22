d93937b842384663c85d4eea7d4e35631d9e4589
Jed Watson <jed.watson@me.com>
2015-11-22 15:18:34 +1000

    Cleaning up routes & middleware

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ keystone.set('500', function(err, req, res, next) {
 var routes = {
 	api: importRoutes('./api'),
 	views: importRoutes('./views'),
-	auth: importRoutes('./auth')
+	auth: importRoutes('./auth'),
 };
 
 // Bind Routes
@@ -40,16 +40,16 @@ exports = module.exports = function(app) {
 	// Browserification
 	app.get('/js/packages.js', browserify(clientConfig.packages, {
 		cache: true,
-		precompile: true
+		precompile: true,
 	}));
 
 	app.use('/js', browserify('./client/scripts', {
 		external: clientConfig.packages,
-		transform: ['babelify']
+		transform: ['babelify'],
 	}));
-	
+
 	// Allow cross-domain requests (development only)
-	if (process.env.NODE_ENV != 'production') {
+	if (process.env.NODE_ENV !== 'production') {
 		console.log('------------------------------------------------');
 		console.log('Notice: Enabling CORS for development.');
 		console.log('------------------------------------------------');
@@ -60,7 +60,7 @@ exports = module.exports = function(app) {
 			next();
 		});
 	}
-	
+
 	// Website
 	app.get('/', routes.views.index);
 	app.get('/meetups', routes.views.meetups);
@@ -78,19 +78,19 @@ exports = module.exports = function(app) {
 	app.get('/mentoring', routes.views.mentoring);
 
 	app.get('/showbag', routes.views.showbag);
-	
+
 	// Session
 	app.all('/join', routes.views.session.join);
 	app.all('/signin', routes.views.session.signin);
 	app.get('/signout', routes.views.session.signout);
 	app.all('/forgot-password', routes.views.session['forgot-password']);
 	app.all('/reset-password/:key', routes.views.session['reset-password']);
-	
+
 	// Authentication
 	app.all('/auth/confirm', routes.auth.confirm);
 	app.all('/auth/app', routes.auth.app);
 	app.all('/auth/:service', routes.auth.service);
-	
+
 	// User
 	app.all('/me*', middleware.requireUser);
 	app.all('/me', routes.views.me);
@@ -99,16 +99,16 @@ exports = module.exports = function(app) {
 
 	// Tools
 	app.all('/notification-center', routes.views.tools['notification-center']);
-	
+
 	// Maintenace
 	app.all('/maintenance', routes.views.maintenance);
-	
+
 	// API
 	app.all('/api*', keystone.middleware.api);
 	app.all('/api/me/meetup', routes.api.me.meetup);
 	app.all('/api/stats', routes.api.stats);
 	app.all('/api/meetup/:id', routes.api.meetup);
-	
+
 	// API - App
 	app.all('/api/app/status', routes.api.app.status);
 	app.all('/api/app/rsvp', routes.api.app.rsvp);
--- routes/middleware.js
+++ routes/middleware.js
@@ -1,6 +1,6 @@
-var _ = require('underscore'),
-	querystring = require('querystring'),
-	keystone = require('keystone');
+var _ = require('underscore');
+var querystring = require('querystring');
+var keystone = require('keystone');
 
 
 /**
@@ -8,9 +8,9 @@ var _ = require('underscore'),
 */
 
 exports.initLocals = function(req, res, next) {
-	
+
 	var locals = res.locals;
-	
+
 	locals.navLinks = [
 		{ label: 'Home',		key: 'home',		href: '/' },
 		{ label: 'About',		key: 'about',		href: '/about' },
@@ -19,22 +19,22 @@ exports.initLocals = function(req, res, next) {
 		{ label: 'Blog',		key: 'blog',		href: '/blog' },
 		{ label: 'Showbag',		key: 'showbag',		href: '/showbag' }
 	];
-	
+
 	locals.user = req.user;
-	
+
 	locals.basedir = keystone.get('basedir');
-	
+
 	locals.page = {
 		title: 'SydJS',
 		path: req.url.split("?")[0] // strip the query - handy for redirecting back to the page
 	};
-	
+
 	locals.qs_set = qs_set(req, res);
-	
+
 	if (req.cookies.target && req.cookies.target == locals.page.path) res.clearCookie('target');
-	
+
 	var bowser = require('../lib/node-bowser').detect(req);
-	
+
 	locals.system = {
 		mobile: bowser.mobile,
 		ios: bowser.ios,
@@ -42,9 +42,9 @@ exports.initLocals = function(req, res, next) {
 		ipad: bowser.ipad,
 		android: bowser.android
 	}
-	
+
 	next();
-	
+
 };
 
 
@@ -53,14 +53,12 @@ exports.initLocals = function(req, res, next) {
 */
 
 exports.loadSponsors = function(req, res, next) {
-	
 	keystone.list('Organisation').model.find().sort('name').exec(function(err, sponsors) {
 		if (err) return next(err);
 		req.sponsors = sponsors;
 		res.locals.sponsors = sponsors;
 		next();
 	});
-	
 }
 
 
@@ -69,7 +67,6 @@ exports.loadSponsors = function(req, res, next) {
 */
 
 exports.initErrorHandlers = function(req, res, next) {
-	
 	res.err = function(err, title, message) {
 		res.status(500).render('errors/500', {
 			err: err,
@@ -77,16 +74,13 @@ exports.initErrorHandlers = function(req, res, next) {
 			errorMsg: message
 		});
 	}
-	
 	res.notfound = function(title, message) {
 		res.status(404).render('errors/404', {
 			errorTitle: title,
 			errorMsg: message
 		});
 	}
-	
 	next();
-	
 };
 
 
@@ -95,18 +89,14 @@ exports.initErrorHandlers = function(req, res, next) {
 */
 
 exports.flashMessages = function(req, res, next) {
-	
 	var flashMessages = {
 		info: req.flash('info'),
 		success: req.flash('success'),
 		warning: req.flash('warning'),
 		error: req.flash('error')
 	};
-	
 	res.locals.messages = _.any(flashMessages, function(msgs) { return msgs.length }) ? flashMessages : false;
-	
 	next();
-	
 };
 
 /**
@@ -114,14 +104,12 @@ exports.flashMessages = function(req, res, next) {
  */
 
 exports.requireUser = function(req, res, next) {
-	
 	if (!req.user) {
 		req.flash('error', 'Please sign in to access this page.');
 		res.redirect('/signin');
 	} else {
 		next();
 	}
-	
 }
 
 /**
@@ -129,14 +117,12 @@ exports.requireUser = function(req, res, next) {
  */
 
 exports.restrictSite = function(req, res, next) {
-	
 	if (!req.user) {
-		if (req.url != '/maintenance') return res.redirect('/maintenance');
+		if (req.url === '/maintenance') return res.redirect('/maintenance');
 		next();
 	} else {
 		next();
 	}
-	
 }
 
 /**
@@ -145,11 +131,8 @@ exports.restrictSite = function(req, res, next) {
 */
 
 var qs_set = exports.qs_set = function(req, res) {
-
 	return function qs_set(obj) {
-
 		var params = _.clone(req.query);
-
 		for (var i in obj) {
 			if (obj[i] === undefined || obj[i] === null) {
 				delete params[i];
@@ -157,11 +140,7 @@ var qs_set = exports.qs_set = function(req, res) {
 				params[i] = obj[i];
 			}
 		}
-
 		var qs = querystring.stringify(params);
-
 		return req.path + (qs ? '?' + qs : '');
-
 	}
-
 }
