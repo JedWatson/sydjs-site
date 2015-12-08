392af04404278397402903791d34405be5882e6f
Albert Still <albertjamesstill@gmail.com>
2015-12-08 18:27:04 +1100

    WIP add `print-graphql-schema` script to improve DX
    Prints user readable type system shorthand of schema to help DX

---
+++ graphql/scripts/printSchema.js
@@ -0,0 +1,11 @@
+// TODO require the parts of Keystone needed for this script to work
+var fs = require('fs');
+var path = require('path');
+var schema = require('../schema');
+var printSchema = require('graphql/utilities').printSchema;
+
+// Save user readable type system shorthand of schema to help DX
+fs.writeFileSync(
+  path.join(__dirname, '../schema.graphql'),
+  printSchema(schema)
+);
