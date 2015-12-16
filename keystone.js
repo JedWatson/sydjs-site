63db09b986b76b13836e6583ede5373a25f9aca9
Jed Watson <jed.watson@me.com>
2015-12-16 18:50:34 +1100

    less specific only spec for babel-register

--- keystone.js
+++ keystone.js
@@ -1,5 +1,5 @@
 // Note it's checking the regex against an absoloute path
-require('babel-register')({ only: /\/sydjs-site\/graphql\/.*/ });
+require('babel-register')({ only: /\/graphql\/.*/ });
 
 // Load .env for development environments
 require('dotenv').load();
