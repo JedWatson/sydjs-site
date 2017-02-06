11280e50964e721fd882b5e43039b2486e13c7a5
Sharkie <craig.sharkie@gmail.com>
2017-02-06 16:06:45 +1100

    Adding internal links to Organiser profile blocks

--- routes/views/about.js
+++ routes/views/about.js
@@ -9,9 +9,9 @@ exports = module.exports = function(req, res) {
 	locals.page.title = 'About SydJS';
 
 	locals.organisers = [
-		{ name: 'Craig Sharkie', image: '/images/organisers/sharkie_400_round.png', twitter: 'twalve', title: 'Founder, MC, Coordinator' },
-		{ name: 'Jed Watson', image: '/images/organisers/jedwatson_400_round.png', twitter: 'jedwatson', title: 'Community coordinator' },
-		{ name: 'John van der Loo', image: '/images/organisers/johnvanderloo_400_round.png', twitter: 'geekyjohn', title: 'Atlassian coordinator' }
+		{ name: 'Craig Sharkie', image: '/images/organisers/sharkie_400_round.png', twitter: 'twalve', title: 'Founder, MC, Coordinator', profile: '/member/sharkie' },
+		{ name: 'Jed Watson', image: '/images/organisers/jedwatson_400_round.png', twitter: 'jedwatson', title: 'Community coordinator', profile: '/member/jed-watson' },
+		{ name: 'John van der Loo', image: '/images/organisers/johnvanderloo_400_round.png', twitter: 'geekyjohn', title: 'Atlassian coordinator', profile: '/member/john-van-der-loo' }
 	]
 
 	view.render('site/about');
