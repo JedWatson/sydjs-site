ab5649b95ac16f2860f4d94754d3b4a1cab78451
Sharkie <craig.sharkie@gmail.com>
2017-02-06 12:33:14 +1100

    Updating Site and Community roles

--- routes/views/about.js
+++ routes/views/about.js
@@ -1,21 +1,19 @@
 var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
-	
+
 	var view = new keystone.View(req, res),
 		locals = res.locals;
-	
+
 	locals.section = 'about';
 	locals.page.title = 'About SydJS';
-	
+
 	locals.organisers = [
-		{ name: 'Craig Sharkie', image: '/images/organiser-craig_sharkie.jpg', twitter: 'twalve',       title: 'Founder, MC, coordinator' },
-		// { name: 'Thinkmill', image: '/images/organiser-thinkmill.jpg',     twitter: 'thethinkmill', title: 'Site coordinator' },
-		{ name: 'Gil Davidson',     image: '/images/organiser-gil_davidson.jpg',     twitter: 'iamnotyourbroom',   title: 'Atlassian coordinator' },
-		{ name: 'Adam Ahmed',    image: '/images/organiser-adam_ahmed.jpg',    twitter: 'hitsthings',   title: 'Atlassian coordinator' },
-		{ name: 'Lachlan Hardy', image: '/images/organiser-lachlan_hardy.jpg', twitter: 'lachlanhardy', title: 'Community coordinator' }
+		{ name: 'Craig Sharkie', image: '/images/organisers/sharkie_400_round.png', twitter: 'twalve', title: 'Founder, MC, Coordinator' },
+		{ name: 'Jed Watson', image: '/images/organisers/jedwatson_400_round.png', twitter: 'jedwatson', title: 'Community coordinator' },
+		{ name: 'John van der Loo', image: '/images/organisers/johnvanderloo_400_round.png', twitter: 'geekyjohn', title: 'Atlassian coordinator' }
 	]
-	
+
 	view.render('site/about');
-	
+
 }
