30ea2873dd39fb00093276e4d207a2eeb31ee904
Esref Durna <edurna@gmail.com>
2017-06-06 04:46:42 +0000

    eslint fixes: keystone.js    4:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines    7:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   12:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   16:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   19:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   21:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   25:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   29:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   33:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   35:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   38:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   44:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   46:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   49:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   52:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   54:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   56:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   58:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   60:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   73:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   77:3  warning  Expected { after 'if' condition      curly   78:3  warning  Expected { after 'if' condition      curly   82:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines   89:1  warning  More than 0 blank lines not allowed  no-multiple-empty-lines
    ✖ 24 problems (0 errors, 24 warnings)

--- keystone.js
+++ keystone.js
@@ -1,63 +1,44 @@
 // Load the babel-register plugin for the graphql directory
 // Note this checks the regex against an absoloute path
 require('babel-register')({ only: /\/graphql\/.*/ });
-
 // Load .env config for development environments
 require('dotenv').config({ silent: true });
-
 // Initialise New Relic if an app name and license key exists
 if (process.env.NEW_RELIC_APP_NAME && process.env.NEW_RELIC_LICENSE_KEY) {
 	require('newrelic');
 }
-
 /**
  * Application Initialisation
  */
-
 var keystone = require('keystone');
 var pkg = require('./package.json');
-
 keystone.init({
-
 	'name': 'SydJS',
 	'brand': 'SydJS',
 	'back': '/me',
-
 	'favicon': 'public/favicon.ico',
 	'less': 'public',
 	'static': 'public',
-
 	'views': 'templates/views',
 	'view engine': 'jade',
 	'view cache': false,
-
 	'emails': 'templates/emails',
-
 	'auto update': true,
 	'mongo': process.env.MONGO_URI || 'mongodb://localhost/' + pkg.name,
-
 	'session': true,
 	'session store': 'mongo',
 	'auth': true,
 	'user model': 'User',
 	'cookie secret': process.env.COOKIE_SECRET || 'sydjs',
-
 	'mandrill api key': process.env.MANDRILL_KEY,
-
 	'google api key': process.env.GOOGLE_BROWSER_KEY,
 	'google server api key': process.env.GOOGLE_SERVER_KEY,
-
 	'ga property': process.env.GA_PROPERTY,
 	'ga domain': process.env.GA_DOMAIN,
-
 	'basedir': __dirname
-
 });
-
 keystone.import('models');
-
 keystone.set('routes', require('./routes'));
-
 keystone.set('locals', {
 	_: require('lodash'),
 	moment: require('moment'),
@@ -70,21 +51,22 @@ keystone.set('locals', {
 	ga_property: keystone.get('ga property'),
 	ga_domain: keystone.get('ga domain')
 });
-
 keystone.set('email locals', {
 	utils: keystone.utils,
 	host: (function() {
-		if (keystone.get('env') === 'staging') return 'http://sydjs-beta.herokuapp.com';
-		if (keystone.get('env') === 'production') return 'http://www.sydjs.com';
+		if (keystone.get('env') === 'staging') {
+return 'http://sydjs-beta.herokuapp.com';
+}
+		if (keystone.get('env') === 'production') {
+return 'http://www.sydjs.com';
+}
 		return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '3000');
 	})()
 });
-
 keystone.set('nav', {
 	'meetups': ['meetups', 'talks', 'rsvps'],
 	'members': ['users', 'organisations'],
 	'posts': ['posts', 'post-categories', 'post-comments'],
 	'links': ['links', 'link-tags', 'link-comments']
 });
-
 keystone.start();
